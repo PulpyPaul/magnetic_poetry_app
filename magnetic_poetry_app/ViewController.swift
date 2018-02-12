2892f094ed3a61469dfdb6bd5cbe1c8468a2a09b
Student <student@howard05.istlabs.rit.edu>
2018-02-11 21:25:54 -0500

    Intial Commit

---
+++ magnetic_poetry_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  magnetic_poetry_app
+//
+//  Created by Student on 2/11/18.
+//  Copyright © 2018 Paul DeSimone Nick Federico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
